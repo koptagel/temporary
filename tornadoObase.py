e0daf4ed17642ea7ee222b1b9a2f03fb9d9e3c59
Hazal Koptagel <koptagelhazal@gmail.com>
2016-04-27 16:43:55 +0300

    1

--- tornadoObase.py
+++ tornadoObase.py
@@ -124,16 +124,20 @@ class MainPage(tornado.web.RequestHandler):
         self.post()
         
     def post(self):
-        self.write('<html><head><h1> Tornado Obase Server </h1></head><br>'
+        self.write('<html><head><h1> Obase Tornado Server </h1></head><br>'
                    '<body><h2> Example Usages </h2><br>'
                    '<b> 45.55.237.86:8880/customerInfo/110236 </b> <br>'
                    'Displays demographic information of customer with id = 110236 <br>'
-                   'Some customer ids to use for customerInfo: 110236, 100240, 110236 <br><br>'
+                   'Some customer ids to use for customerInfo: 110236, 100240 <br><br>'
                    '<b> 45.55.237.86:8880/customerSale/99888001 </b> <br>'
                    'Displays heatmap of the sales of customer with id = 99888001 <br>'
-                   'Some customer ids to use for customerSale: 99888001, 991921217, 99958429 <br>'
+                   'Some customer ids to use for customerSale: 99888001, 991921217, 99958429 <br><br>'
+                   '<b> 45.55.237.86:8880/customerSaleJson/99888001 </b> <br>'
+                   'Displays the image url the sales matrix of customer with id = 99888001 in JSON format <br>'
+                   'Some customer ids to use for customerSaleJson: 99888001, 991921217, 99958429 <br><br>'
                    '</body></html>')
-       
+        
+        
 
 # Code segment that will return customer demographic information as a JSON object. 
 # The customer id is given. 
@@ -147,7 +151,8 @@ class CustomerInfo(tornado.web.RequestHandler):
         #self.write("Customer Id: %d " % customerId)
         
         r = requests.get('http://212.57.2.68:93/api/database/musteri?$filter=IdMusteri+eq+%d'%customerId)
-        info = r.json()
+
+        info = json.dumps(r.json())
 
         #self.write("Customer Demographic Information: %s" % info)
         self.write("%s" % info)
@@ -162,38 +167,59 @@ class CustomerSale(tornado.web.RequestHandler):
         customerId = int(parameter)
         customerIndex = np.where(selectedCustomerIndex2Id==customerId)[0][0]
         
-        #SalesTensor = loadFundamentalTensorCustomer('matfiles/AllHours_Item_Customer_Tensor.mat', customerIndex)
-        #SalesTensor = collapseTensor(SalesTensor, [1,0,0,1,0], 'sum')
+        X = loadFundamentalTensorCustomer('files/AllHours_Item_Customer_Tensor.mat', customerIndex)
+        X = collapseTensor(X, [1,0,0,1,0], 'sum')
+        SalesTensor = X[0,:,:,0,0] 
+        
+        plt.figure
+        plt.imshow(SalesTensor, aspect='auto', interpolation='nearest',vmin=0, vmax=100)
+        plt.savefig('./files/%d.png' % customerId)
+        
+
+        self.write('<html>'
+                   'Sale Matrix of Customer %d <br>'
+                   '<img src=\"/files/%d.png\"><br></body></html>' % (customerId,customerId))
+        
+        imageUrl = ("%s:%s/files/%d.png" % (HOST,PORT,customerId))
+        
+        info = json.dumps({"IdMusteri": customerId, "image_url": imageUrl})
+        self.write("%s" % info)
+        
+# Given the customer id, this code segment get the sales matrix of the customer, saves it in .png format and displays the json. 
+class CustomerSaleJson(tornado.web.RequestHandler):
+    def get(self, parameter):
+        self.post(parameter)
+    
+    def post(self, parameter):
+        customerId = int(parameter)
+        customerIndex = np.where(selectedCustomerIndex2Id==customerId)[0][0]
         
         X = loadFundamentalTensorCustomer('files/AllHours_Item_Customer_Tensor.mat', customerIndex)
         X = collapseTensor(X, [1,0,0,1,0], 'sum')
         SalesTensor = X[0,:,:,0,0] 
-	
-	#global X
-        #SalesTensor = X[:,:,customerIndex]
         
         plt.figure
         plt.imshow(SalesTensor, aspect='auto', interpolation='nearest',vmin=0, vmax=100)
         plt.savefig('./files/%d.png' % customerId)
 
-        self.write('<html>'
-                   'Sale Matrix of Customer %d <br>'
-                   '<img src=\"/files/%d.png\"></body></html>' % (customerId,customerId))
-                   #'<img src=\"/files/%d.png\"></body></html>' % (customerIndex,customerIndex))
-
-
+        imageUrl = ("%s:%s/files/%d.png" % (HOST,PORT,customerId))
+        
+        info = json.dumps({"IdMusteri": customerId, "image_url": imageUrl})
+        self.write("%s" % info)
+            
 # The configuration of routes.
 routes_config = [
     (r"/", MainPage), 
     (r"/customerInfo/([^/]+)", CustomerInfo),
     (r"/customerSale/([^/]+)", CustomerSale),
+    (r"/customerSaleJson/([^/]+)", CustomerSaleJson),
     (r"/(.*\.png)", tornado.web.StaticFileHandler,{"path": "." }),
 ]
 application = tornado.web.Application(routes_config)
 
 
 def start():
-    print("Tornado Obase Server.\nStarting on port %s" % PORT)
+    print("Obase Tornado Server.\nStarting on host %s, port %s" % (HOST,PORT))
     http_server = HTTPServer(application, xheaders=True)
     http_server.listen(PORT)
     tornado.ioloop.IOLoop.instance().start()
