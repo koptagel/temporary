6d80f5a1bf2068cd62100f8b13a303392fca87d5
A. Taylan Cemgil <taylan.cemgil@boun.edu.tr>
2016-08-29 11:02:00 +0300

    port updated host to 212.57.2.68

--- tornadoObase.py
+++ tornadoObase.py
@@ -46,7 +46,7 @@ from TxtFileFuncs import loadRecommendationOfCustomerMatrixFromTxt2
 from TxtFileFuncs import loadRecommendationOfCustomerProfilesFromTxt
 
 # Setting host name, port number, directory name and the static path. 
-HOST = '0.0.0.0'
+HOST = '212.57.2.68'
 PORT = 8086
 
 CUSTOMERCOUNT = 3193
